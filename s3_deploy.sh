7595bdf39146f74c03958d8bc540ca8d48a5a588
Ying Wang <yingw787.personal@gmail.com>
2020-10-13 09:53:27 -0400

    Resume update: 2020-10-13T13:53:27Z

--- s3_deploy.sh
+++ s3_deploy.sh
@@ -4,5 +4,5 @@ git add .
 git commit -m "Resume update: $current_time"
 git push origin master
 
-aws s3 rm s3://resume.yingw787.com --recursive --profile s3_personal
-aws s3 cp resume.pdf s3://resume.yingw787.com --profile s3_personal
+aws s3 rm s3://resume.yingw787.com --recursive --profile ying.wang
+aws s3 cp resume.pdf s3://resume.yingw787.com --profile ying.wang
